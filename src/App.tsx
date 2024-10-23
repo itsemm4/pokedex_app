e2d496446703299d78c9e148177ac7ce5195c011
EMMA ROBINET <emma.rbnt@gmail.com>
2024-10-23 22:23:53 +0200

    add two Pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import "./App.css";
 
-import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  return (
-      <div>
-        <PokemonCard />
-      </div>
-  );
+	return (
+		<div>
+			<PokemonCard />
+		</div>
+	);
 }
 
-  export default App;
+export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,27 @@
 function PokemonCard() {
-    return (
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="Bulbasaur"
-        />
+	const pokemon = pokemonList[0];
 
-    <figcaption>Bulbaseur</figcaption>
-    </figure>
-  );
+	return (
+		<figure>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
 }
-  
-  export default PokemonCard;
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
+export default PokemonCard;
